1c826f6a0a2afc4bc86da523e0fd1ab674ce685a
Mike Cecconello <mikececco2000@gmail.com>
2023-03-26 12:16:20 +0200

    added validation to pass tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :title, :description, :price, presence: true
+  validates :title, :description, :price, uniqueness: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -21,4 +21,34 @@ RSpec.describe Product, type: :model do
       expect(new_product.errors[:title]).to include('has already been taken')
     end
   end
+
+  describe 'description' do
+    it 'validates presence of description' do
+      product = @invalid_product
+      product.valid?
+      expect(product.errors[:description]).to include("can't be blank")
+    end
+
+    it 'validates uniqueness of a description' do
+      @valid_product.save
+      new_product = @valid_product.dup
+      new_product.valid?
+      expect(new_product.errors[:description]).to include('has already been taken')
+    end
+  end
+
+  describe 'price' do
+    it 'validates presence of price' do
+      product = @invalid_product
+      product.valid? # triggers validation in the price model
+      expect(product.errors[:price]).to include("can't be blank")
+    end
+
+    it 'validates uniqueness of a price' do
+      @valid_product.save
+      new_product = @valid_product.dup
+      new_product.valid?
+      expect(new_product.errors[:price]).to include('has already been taken')
+    end
+  end
 end
